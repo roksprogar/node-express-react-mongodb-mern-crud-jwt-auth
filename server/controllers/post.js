d94eec34e8027a3d57f591e482fa569647370023
Rok Sprogar <rok.sprogar@gmail.com>
2022-07-24 00:31:05 +0200

    post update and destroy routes

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,49 +1,74 @@
-const Post = require('../models/post')
-const slugify = require('slugify')
+const Post = require("../models/post");
+const slugify = require("slugify");
 
 exports.create = (req, res) => {
-    const {title, content, user} = req.body
-    const slug = slugify(title, {
-        lower: true
-    })
+  const { title, content, user } = req.body;
+  const slug = slugify(title, {
+    lower: true,
+  });
 
-    // Validation.
-    switch(true) {
-        case !title:
-        return res.status(400).json({error: 'Title is required!'})
-        break
-        case !content:
-        return res.status(400).json({error: 'Content is required!'})
-        break
+  // Validation.
+  switch (true) {
+    case !title:
+      return res.status(400).json({ error: "Title is required!" });
+      break;
+    case !content:
+      return res.status(400).json({ error: "Content is required!" });
+      break;
+  }
+
+  // Create post.
+  Post.create({ title, content, user, slug }, (err, post) => {
+    if (err) {
+      console.log(err);
+      res.status(400).json({
+        error: "Most likely a duplicate post, please try another title!",
+      });
     }
 
-    // Create post.
-    Post.create({title, content, user, slug}, (err, post) => {
-        if(err) {
-            console.log(err)
-            res.status(400).json({error: 'Most likely a duplicate post, please try another title!'})
-        }
-
-        res.json(post)
-    })
-}
+    res.json(post);
+  });
+};
 
 exports.list = (req, res) => {
-    Post.find({})
+  Post.find({})
     .limit(10)
     .sort({ createdAt: -1 })
     .exec((err, posts) => {
-        if(err) console.log(err)
-        res.json(posts)
-    })
-}
+      if (err) console.log(err);
+      res.json(posts);
+    });
+};
 
 exports.show = (req, res) => {
-    const {slug} = req.params
+  const { slug } = req.params;
 
-    Post.findOne({slug})
-    .exec((err, post) => {
-        if(err) console.log(err)
-        res.json(post)
-    })
-}
+  Post.findOne({ slug }).exec((err, post) => {
+    if (err) console.log(err);
+    res.json(post);
+  });
+};
+
+exports.update = (req, res) => {
+  const { slug } = req.params;
+  const { title, content, user } = req.body;
+
+  Post.findByIdAndUpdate(
+    { slug },
+    { title, content, user },
+    { new: true }
+  ).exec((err, post) => {
+    if (err) console.log(err);
+    res.json(post);
+  });
+};
+
+exports.destroy = (req, res) => {
+  const { slug } = req.params;
+  Post.findOneAndRemove({ slug }).exec((err, post) => {
+    if (err) console.log(err);
+    res.json({
+      message: "Post deleted",
+    });
+  });
+};
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
 // Import controller methods.
-const { create, list } = require('../controllers/post')
+const { create, list, show, update, destroy } = require("../controllers/post");
 
-router.get('/', list);
-router.post('/create', create);
+router.get("/", list);
+router.post("/create", create);
+router.get("/post/:slug", show);
+router.put("/post/:slug", update);
+router.delete("/post/:slug", destroy);
 
 module.exports = router;
